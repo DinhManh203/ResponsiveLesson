47993a89e3a601b1882609891de106a6b12d33f6
DinhManh203 <dinhmanhoke203@gmail.com>
2025-04-16 15:45:29 +0700

    new_update_16/4/2025

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from 'react';
+import UserCard from "@/components/UserCard";
+import CountChart from "@/components/CountChart"
+import AttendanceChart from "@/components/AttendanceChart"
+import FinanceChart from "@/components/FinanceChart"
 
 const AdminPage = () => {
   return (
-    <div>
-      Admin Page
+    <div className='p-4 flex gap-4 flex-col md:flex-row'>
+      {/* Left */}
+      <div className='w-full lg:w-2/3 flex flex-col gap-4'>
+        {/* User Cards */}
+        <div className='flex gap-4 justify-between flex-wrap'>
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
+        </div>
+        {/* Middle charts */}
+        <div className='flex gap-4 flex-col lg:flex-row'>
+          {/* Count Chart */}
+          <div className='w-full lg:w-1/3 h-[450px]'>
+            <CountChart />
+          </div>
+
+          {/* Attendance Chart */}
+          <div className='w-full lg:w-2/3 h-[450px]'>
+            <AttendanceChart />
+          </div>
+        </div>
+
+        {/* Bottom charts */}
+        <div className='w-full h-[500px]'>
+            <FinanceChart />
+        </div>
+      </div>
+
+      {/* Right */}
+      <div className='w-full lg:w-1/3 '>
+        R
+      </div>
     </div>
   )
 }
--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -11,7 +11,7 @@ export default function DashboardLayout({
     return (
         <div className="h-screen flex">
             {/* Left */}
-            <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200 p-4">
+            <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-white p-4">
               <Link href="/" className="flex items-center justify-center lg:justify-start gap-2"> 
                 <Image src="/logo.png" width={32} height={32} alt={""} />
                 <span className="hidden lg:block ">SchoolLama</span>
